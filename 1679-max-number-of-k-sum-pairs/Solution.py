4be49872a743ea409626e043ccedb38cf07ead5d
Soltan Soltanli <65287059+s-s01tan@users.noreply.github.com>
2025-07-24 23:58:18 -0700

    1679. Max Number of K-Sum Pairs

---
+++ 1679-max-number-of-k-sum-pairs/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        result = 0
+        num_dict = {}
+        
+        for num in nums:
+            to_complete = k - num
+            if num_dict.get(to_complete, 0) > 0:
+                num_dict[to_complete] -= 1
+                result += 1
+            else:
+                num_dict[num] = num_dict.get(num, 0) + 1
+        return result
